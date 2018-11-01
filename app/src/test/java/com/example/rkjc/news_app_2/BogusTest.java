0b9a3c992fa747a36c57bfa46fde8af5b1a0c7dd
rkjc <richardkjcross@gmail.com>
2018-11-01 09:08:23 -0700

    added BoogusTest alwayPass and increased emulator level to 22

---
+++ app/src/test/java/com/example/rkjc/news_app_2/BogusTest.java
@@ -0,0 +1,16 @@
+package com.example.rkjc.news_app_2;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertTrue;
+
+public class BogusTest {
+
+    @Test
+    public void AlwaysPassTest(){
+
+        assertTrue(true);
+    }
+}
